8ea726cd4a26e2d9f1a53a724d9344a5de04ab7b
AGonzales-9R <gonzales.am@obmontessori.edu.ph>
2025-03-12 15:22:23 +0800

    Update guide.js

--- guide.js
+++ guide.js
@@ -15,52 +15,24 @@ function userName(){
 }
 
 // FOR CHECKING FARE PRICE
-	// FARE PRICE
-	function getFarePrice(){
-	// TO GET THE VALUES CHOSEN BY THE USER FROM THE DROPDOWNS 
-		var leaving1 = document.getElementById('inputLeaving').value;
-		var discount1 = document.getElementById('inputDiscount').value;
-	// PLACEHOLDER FOR THE TOTAL FARE PRICE
-		var totalPrice1;
+	if (leaving1 == "na" && going1 == "qa"||leaving1 == "na" && going1 == "kam"){
+			journey = "Thirteen";
+			
+		} else if (leaving1 == "" && going1 == ""||leaving1 == "" && going1 == ""){
+			journey = "Sixteen";
+		
+		} else if (leaving1 == "" && going1 == ""||leaving1 == "" && going1 == ""){
+			journey = "Twenty";
+		
+		} else if (leaving1 == "" && going1 == ""||leaving1 == "" && going1 == ""){
+			journey = "TwentyFour";
+		
+		} else if (leaving1 == "" && going1 == ""||leaving1 == "" && going1 == ""){
+			journey = "TwentyEight";
 
-	// TO KNOW WHEN TO APPLY THE DISCOUNT
-		if (discount1 == "regular"){
-			totalPrice1 = (fare1);
-			window.alert('The total fare price is ' + totalPrice1 +'. ' + 'Click the "Buy Ticket" button to purchase the ticket.').innerHTML;
-								
-		} else {
-		// COMPUTING FOR THE DISCOUNTED PRICE
-		// .80 IS THE SHORTCUT OF GETTING 20% DISCOUNT OFF THE REGULAR PRICE
-			totalPrice1 = (fare1*.80);
-			window.alert('The total fare price is ' + totalPrice1 +'. ' + 'Click the "Buy Ticket" button to purchase the ticket.').innerHTML;
-		}
-
-							
-		}
-
-	// TO BUY THE TICKET
-	// TICKET PRICE
-	function buyTicket(){
-	// TO GET THE VALUES CHOSEN BY THE USER FROM THE DROPDOWNS 
-		var leaving2 = document.getElementById('inputLeaving').value;
-		var discount2 = document.getElementById('inputDiscount').value;
-		// PLACEHOLDER FOR THE TOTAL FARE PRICE
-		var totalPrice2;
-
-		// TO KNOW WHEN TO APPLY THE DISCOUNT
-		if (discount2 == "regular"){
-			totalPrice2 = (fare2);
-			window.alert('The total ticket price for your selection is ' + totalPrice2 +'. ' + 'Proceed to the next station.').innerHTML;
-								
-		} else {
-		// COMPUTING FOR THE DISCOUNTED PRICE
-		// .80 IS THE SHORTCUT OF GETTING 20% DISCOUNT OFF THE REGULAR PRICE
-			totalPrice2 = (fare2*.80);
-			window.alert('The total ticket price for your selection is ' + totalPrice2 +'. ' + 'Proceed to the next station.').innerHTML;
-		}
-
-							
-		}
+		} else if (leaving1 == "" && going1 == ""||leaving1 == "" && going1 == ""){
+			window.alert("Pls choose a different Destination").innerHTML;
+	}
 
 // PRINT TICKET
 function ticketPrint(){
